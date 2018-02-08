9461a3a61fa8b6e11f1b4563f619cbff652cd8c0
Aliaksandr Kharytaniuk <aliaksandr2kharytaniuk@gmail.com>
2018-02-09 01:35:15 +0300

    Removed main view and added empty cities view

--- CityMap/CityMap/App.xaml.cs
+++ CityMap/CityMap/App.xaml.cs
@@ -59,7 +59,7 @@ namespace CityMap
 					// When the navigation stack isn't restored navigate to the first page,
 					// configuring the new page by passing required information as a navigation
 					// parameter
-					rootFrame.Navigate(typeof(MainView), e.Arguments);
+					rootFrame.Navigate(typeof(CitiesView), e.Arguments);
 				}
 				// Ensure the current window is active
 				Window.Current.Activate();

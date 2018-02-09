d8d39c39bdd778a6bf2d84caf25d9f6e4f4f2240
Aliaksandr Kharytaniuk <aliaksandr2kharytaniuk@gmail.com>
2018-02-09 12:26:46 +0300

    Read data from local storage folder in case we have no data from application api service

--- CityMap/CityMap/Services/CitiesService.cs
+++ CityMap/CityMap/Services/CitiesService.cs
@@ -2,16 +2,19 @@
 using System.Threading.Tasks;
 using CityMap.Models;
 using CityMap.Services.Api;
+using CityMap.Infrastructure;
 
 namespace CityMap.Services
 {
 	public class CitiesService
 	{
 		private readonly ApplicationApiService _applicationApiService;
+		private LocalSettingsStorage _localSettingsStorage;
 
 		public CitiesService()
 		{
 			_applicationApiService = new ApplicationApiService();
+			_localSettingsStorage = new LocalSettingsStorage();
 		}
 
 		/// <summary>
@@ -21,6 +24,16 @@ namespace CityMap.Services
 		public async Task<IEnumerable<City>> LoadCitiesAsync()
 		{
 			var applicationData = await _applicationApiService.FetchDataAsync();
+
+			if (applicationData.Cities != null)
+			{
+				await _localSettingsStorage.SaveApplicationData(applicationData);
+			}
+			else
+			{
+				applicationData = await _localSettingsStorage.GetApplicationData();
+			}
+
 			return applicationData.Cities;
 		}
 	}

5e80a2b4a7820913bfc4703a5ecd8efdc88efd84
Aliaksandr Kharytaniuk <aliaksandr2kharytaniuk@gmail.com>
2018-02-09 10:40:17 +0300

    Added attributes to models

--- CityMap/CityMap/Models/ApplicationData.cs
+++ CityMap/CityMap/Models/ApplicationData.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
 
 namespace CityMap.Models
 {
+	[DataContract()]
 	public class ApplicationData
 	{
+		[JsonProperty]
+		[DataMember(Name = "photos")]
 		public IEnumerable<City> Cities { get; set; }
 	}
 }
--- CityMap/CityMap/Models/City.cs
+++ CityMap/CityMap/Models/City.cs
@@ -1,9 +1,25 @@
-﻿namespace CityMap.Models
+﻿using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace CityMap.Models
 {
+	[DataContract()]
 	public class City
 	{
+		[JsonProperty]
+		[DataMember(Name = "id")]
 		public int Id { get; set; }
+
+		[JsonProperty]
+		[DataMember(Name = "title")]
 		public string Name { get; set; }
+
+		[JsonProperty]
+		[DataMember(Name = "description")]
 		public string Description { get; set; }
+
+		[JsonProperty]
+		[DataMember(Name = "url")]
+		public string ImageUrl { get; set; }
 	}
 }

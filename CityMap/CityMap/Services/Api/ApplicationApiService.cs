8775473e96d56f99af5f0ee2c625f69fecc6de3c
Aliaksandr Kharytaniuk <aliaksandr2kharytaniuk@gmail.com>
2018-02-09 10:50:32 +0300

    Added application api service

---
+++ CityMap/CityMap/Services/Api/ApplicationApiService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Web.Http;
+using Newtonsoft.Json;
+using CityMap.Models;
+
+namespace CityMap.Services.Api
+{
+	public class ApplicationApiService
+	{
+		private const string ApplicationDataUrl = "https://api.myjson.com/bins/7ybe5";
+
+		/// <summary>
+		/// Fetch data from remote service
+		/// </summary>
+		/// <returns>Returns deserialized application data object</returns>
+		public async Task<ApplicationData> FetchDataAsync()
+		{
+			var httpClient = new HttpClient();
+			var applicationDataUri = new Uri(ApplicationDataUrl);
+			var result = new ApplicationData();
+
+			try
+			{
+				var response = await httpClient.GetStringAsync(applicationDataUri);
+				result = JsonConvert.DeserializeObject<ApplicationData>(response);
+			}
+			catch
+			{
+				// Details in ex.Message and ex.HResult.
+			}
+
+			httpClient.Dispose();
+
+			return result;
+		}
+	}
+}
